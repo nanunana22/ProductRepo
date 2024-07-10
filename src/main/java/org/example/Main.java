f61eeade1279f2a6abe75b7528e2ce5bf77b93d1
ursulakohler <kohlerursula22@gmail.com>
2024-07-10 12:23:19 +0200

    Aufgabe1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,26 +5,33 @@ package org.example;
 //Schritt 2: Implementieren Sie die Klasse ProductRepo mit einer Liste zum Speichern von Produkten.
 //Schritt 3: Implementieren Sie Methoden zum Hinzufügen, Entfernen und Abrufen von Produkten (einzelnes Produkt und alle Produkte).
 
+//Erstellen Sie eine Klasse OrderListRepo, die eine Liste von Order-Objekten enthält.
+//Schritt 1: Erstellen Sie einen Datensatz Order mit den erforderlichen Attributen.
+//Schritt 2: Implementieren Sie die Klasse OrderListRepo mit einer Liste zum Speichern von Bestellungen.
+//Schritt 3: Implementieren Sie Methoden zum Hinzufügen, Entfernen und (einzelner Auftrag und alle Aufträge).
 import java.util.ArrayList;
 import java.util.List;
 // Erstellen einer ArrayList
 public class Main {
     public static void main(String[] args) {
-        // Erstellen einer ArrayList
-        List<String> meineListe = new ArrayList<>();
+        ProductRepo repo = new ProductRepo();
+        System.out.println(repo.getProducts());
 
-        // Hinzufügen von Elementen zur Liste
-        meineListe.add("Apfel");
-        meineListe.add("Banane");
-        meineListe.add("Orange");
+        Product milk = new Product("milk",22,3);
+        repo.putProduct(milk);
+        System.out.println(repo.getProducts());
+        repo.removeProduct(milk);
+        System.out.println(repo.getProducts());
+//        List<Product> list = new ArrayList<>();
+//        Product product = new Product("milk", 22,3);
+//        list.add(product);
+//        list.add(new Product("avocado", 12,4));
+//        list.add(new Product("mango", 13,5));
+//        System.out.println(list);
 
-        // Zugriff auf Elemente in der Liste
-        System.out.println("Das erste Element ist: " + meineListe.get(0));
 
-        // Durchlaufen der Liste
-        System.out.println("Alle Elemente in der Liste:");
-        for (String frucht : meineListe) {
-            System.out.println(frucht);
-        }
+
+
+
     }
 }
