f8e9dce170337d1c55bd0e2302fbe196cfa33419
ursulakohler <kohlerursula22@gmail.com>
2024-07-10 10:11:47 +0200

    add ProductRepo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Erstellen Sie eine Klasse ProductRepo, die eine Liste von Product-Objekten enthält.
+//Schritt 1: Erstellen Sie einen Datensatz Product mit den erforderlichen Attributen.
+//Schritt 2: Implementieren Sie die Klasse ProductRepo mit einer Liste zum Speichern von Produkten.
+//Schritt 3: Implementieren Sie Methoden zum Hinzufügen, Entfernen und Abrufen von Produkten (einzelnes Produkt und alle Produkte).
+
+import java.util.ArrayList;
+import java.util.List;
+// Erstellen einer ArrayList
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Erstellen einer ArrayList
+        List<String> meineListe = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Hinzufügen von Elementen zur Liste
+        meineListe.add("Apfel");
+        meineListe.add("Banane");
+        meineListe.add("Orange");
+
+        // Zugriff auf Elemente in der Liste
+        System.out.println("Das erste Element ist: " + meineListe.get(0));
+
+        // Durchlaufen der Liste
+        System.out.println("Alle Elemente in der Liste:");
+        for (String frucht : meineListe) {
+            System.out.println(frucht);
         }
     }
 }
